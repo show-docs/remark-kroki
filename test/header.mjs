5c85f8d2c5de72fe82d428a4b29b5401cab15741
Eric Chen <airkro@qq.com>
2025-10-04 00:31:34 +0800

    test(headers): add unit test

---
+++ test/header.mjs
@@ -0,0 +1,28 @@
+import test from 'ava';
+
+import { TransformSnapshot } from './helper/lib.mjs';
+
+test(
+  'okay',
+  TransformSnapshot,
+  `
+\`\`\`kroki type=plantuml Kroki-Diagram-Options-theme=amiga
+  A --> B
+\`\`\`
+`,
+  { server: 'https://kroki.io' },
+);
+
+test(
+  'alias',
+  TransformSnapshot,
+  `
+\`\`\`plantuml type=mermaid alt=abc Kroki-Diagram-Options-theme=amiga
+  A --> B
+\`\`\`
+`,
+  {
+    server: 'https://kroki.io',
+    alias: ['plantuml'],
+  },
+);
